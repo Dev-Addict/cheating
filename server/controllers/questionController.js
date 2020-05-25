9a0a7aaf607d94dbadfb090a23ec0b38659f71e4
WolfMAN <aria.azadi.pour@protonmail.com>
2020-05-25 21:37:06 +0430

    create push answer

--- server/controllers/questionController.js
+++ server/controllers/questionController.js
@@ -1,5 +1,8 @@
 const factory = require('./handlerFactory');
 const Question = require('../models/Question');
+const Answer = require('../models/Answer');
+const catchRequest = require('../utils/catchRequest');
+const AppError = require('../utils/AppError');
 
 exports.getQuestions = factory.getAll(Question);
 
@@ -10,3 +13,24 @@ exports.getQuestion = factory.getOne(Question);
 exports.updateQuestion = factory.updateOne(Question);
 
 exports.deleteQuestion = factory.deleteOne(Question);
+
+exports.pushAnswer = catchRequest(
+    async (req, res) => {
+        const question = await Question.findById(req.params.id);
+        if (!question) {
+            throw new AppError('No document found with this ID', 404)
+        }
+        const answer = await Answer.create(req.body);
+        question.answers.push(answer._id);
+        const doc = await Question.findByIdAndUpdate(req.params.id, question, {
+            new: true
+        });
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                doc
+            }
+        });
+    }
+);
