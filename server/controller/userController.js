ac559c1ed089d7159b68cbf6099f2bca69f177ff
WolfMAN <aria.azadi.pour@protonmail.com>
2020-05-25 17:49:18 +0430

    create user controller

---
+++ server/controller/userController.js
@@ -0,0 +1,12 @@
+const factory = require('./handlerFactory');
+const User = require('../models/User');
+
+exports.getUsers = factory.getAll(User);
+
+exports.createUser = factory.createOne(User);
+
+exports.getUser = factory.getOne(User);
+
+exports.updateUser = factory.updateOne(User);
+
+exports.deleteUser = factory.deleteOne(User);
--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,3 @@
-const crypto = require('crypto');
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcrypt');
