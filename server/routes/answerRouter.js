dbb9abdce3d87060046a02e2a6a38e3e1020c572
WolfMAN <aria.azadi.pour@protonmail.com>
2020-05-25 22:19:57 +0430

    create answer router

---
+++ server/routes/answerRouter.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const answerController = require('../controllers/answerController');
+const authController = require('../controllers/authController');
+
+const router = express.Router();
+
+router.route('/')
+    .get(authController.protect, answerController.getAnswers)
+    .post(authController.protect, authController.restrictTo('admin'), answerController.createAnswer);
+router.route('/:id')
+    .get(authController.protect, answerController.getAnswer)
+    .patch(authController.protect, authController.restrictTo('admin'), answerController.updateAnswer)
+    .delete(authController.protect, authController.restrictTo('admin'), answerController.deleteAnswer);
+
+module.exports = router;
